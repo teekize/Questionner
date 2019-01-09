a10dbd460de7bd52fbfd4fd05131caef0113294d
Teeka Elvis <elvisteeka1@gmail.com>
2019-01-09 16:15:06 +0300

    [163047963] added changed some features in the test_question file

--- app/api/test/v1/test_question.py
+++ app/api/test/v1/test_question.py
@@ -3,9 +3,10 @@ import unittest
 from flask import jsonify, json
 from app import createapp
 
-class TestQuestionsEndpoint(unittest.Testcase):
+class TestQuestionsEndpoint(unittest.TestCase):
     def setUp(self):
         self.app=createapp()
+        self.app.testing=True
         self.client=self.app.test_client()
         self.q1={
             "id" : 1,#Integer,
@@ -21,29 +22,32 @@ class TestQuestionsEndpoint(unittest.Testcase):
 
     def test_question_post(self):
         '''tests the creation of a question method requires POST method'''
-        response=self.client().post("/questions/", data=json.dumps(self.q1))
-        results=json.loads(reponse.data.decode("utf-8"))
+        response=self.client.post("/questions/", data=json.dumps(self.q1), content_type=("application/json"))
+        results=json.loads(response.data.decode("utf-8"))
         self.assertEqual(results.status_code, 201)
 
     def test_question_patch_upvote(self):
         '''tests the updating of a question requires PATCH method'''
-        response_1=self.client().post("/questions/", data=json.dumps(self.q1))
-        results_1=json.loads(reponse_1.data.decode('utf-8'))
+        response_1=self.client.post("/questions/", data=json.dumps(self.q1))
+        results_1=json.loads(response_1.data.decode('utf-8'))
+        results_id=results_1["id"]
+        # resluts_vote=
         self.assertEqual(results_1.status_code, 201)
 
-        response=self.client().patch("/questions/1/upvote", data=json.dumps({"vote":results_1["vote"]+1})
+        response=self.client.patch("/questions/{}/upvote".format(results_id))
         results=json.loads(response.data.decode('utf-8'))
         self.assertEqual(results.status_code, 200)#the status code for updating
 
     def test_question_patch_downvote(self):
             '''tests the updating of a question requires the PATCH method'''
              
-            response=self.client().post("/questions/", data=json.dumps(self.q1))
-            results=json.loads(reponse.data.decode("utf-8"))
+            response=self.client.post("/questions/", data=json.dumps(self.q1))
+            results=json.loads(response.data.decode("utf-8"))
+            results_id=results["id"]
             self.assertEqual(results.status_code, 201)
 
-            response=self.client().patch("/questions/1/downvote", data=json.dumps({"votes":["votes"]-1}))
+            response=self.client.patch("/questions/{}/downvote".format(results_id))
             results=json.loads(response.data.decode("utf-8"))
-            assertEqual(results.status_code, )#the status code for updating
+            self.assertEqual(results.status_code, 200)#the status code for updating
 
 
