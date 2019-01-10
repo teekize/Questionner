e0360797da07d86752e52de79ed763dbd26646c9
Teeka Elvis <elvisteeka1@gmail.com>
2019-01-10 08:58:59 +0300

    [finish#163094350] added the meetups endpoint and the data model for the meetup

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,9 @@ from flask import Flask
 
 def createapp():
     app=Flask(__name__)
+
     from app.api.v1.meetups.views.views import meetups
+
     app.register_blueprint(meetups)
+    
     return app
--- app/api/v1/meetups/views/views.py
+++ app/api/v1/meetups/views/views.py
@@ -2,23 +2,26 @@ from flask import Blueprint, jsonify, request
 from app.api.v1.meetups.models import meetup #MeetUp, meetups
 import datetime
 
-meetups=Blueprint("__name__", "meetup_blueprint")
+meetups=Blueprint("meetups", __name__) #)
 
 @meetups.route("/meetups", methods=["POST"])
 def create_meetup():
     if not request.json or not "topic" in request.json:
         return jsonify({"message":"Required fields missing"})
     
-    if not "happeningOn" in request.json or not "location" in request.json:
-        return jsonify({"message":"required fileds missing"}) 
+    # if not "happeningOn" in request.json or not "location" in request.json:
+    #     return jsonify({"message":"required fileds missing"}) 
 
-    name = request.json["title"]
+    name = request.json["name"]
     location = request.json["location"]
     topic = request.json["topic"]
-    tags = request.json["tags"]
+    tags = request.json["Tags"]
     images = request.json["images"]
 
-    if meetup.MeetUp.validate_post(images, location, name, topic, tags)==True:
+    if meetup.MeetUp.validate_post(images, location, name, topic, tags)!=True:
+        return jsonify({"message":"Invalid inputs"}), 404
+        
+    else:
         new_meetup={
             "id" : meetup.meet_ups[0]["id"] +1,
             "createdOn" : datetime.datetime.utcnow(),
@@ -30,7 +33,13 @@ def create_meetup():
         }
 
         meetup.meet_ups.append(new_meetup)
-        return jsonify({"message" : [{"id" :new_meetup["id"]}]}), 201
+        return jsonify({"status_code":201, 
+                        "data" : [{"id" :new_meetup["id"], "location":new_meetup["location"]}
+                                 
+                                 ]
+
+                       }
+                        )
 
 
         #name, location, topic, tags, images
