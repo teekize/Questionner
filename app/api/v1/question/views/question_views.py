e65f249f278b94e09436c0aa5c36638cda81b7ee
Teeka Elvis <elvisteeka1@gmail.com>
2019-01-10 11:21:45 +0300

    [163111287] added the post question view, initilialized the question model

---
+++ app/api/v1/question/views/question_views.py
@@ -0,0 +1,39 @@
+from flask import Blueprint, jsonify, request
+from app.api.v1.question.models import question_model#MeetUp, meetups
+import datetime
+
+question_blueprint=Blueprint("question_blueprint", __name__)
+
+@question_blueprint.route("/questions", methods=["POST"])
+def post_question():
+    if not request.json or not "body" in request.json:
+        return jsonify({"status":400,
+                        "message":"Required fields missing"
+            })
+    body = request.json["body"]
+    asked_by = request.json["createdBy"]
+    title = request.json["title"]
+    meetup = request.json["meetup"]
+
+    if question_model.Question_Validators(body, asked_by, title) == True:
+        new_question={
+            "id" : question_model.questions[-1]+1,
+            "createdOn" : datetime.datetime.utcnow(),
+            "createdBy" : asked_by,
+            "body" : body,
+            "title" : title,
+            "meetup" : meetup
+        }
+
+        question_model.questions.append(new_question)
+        return jsonify(
+            {"status":201,
+            "data":[
+                {"user": new_question["createdBy"],
+                 "title" : new_question["title"],
+                 "body" : new_question["body"],
+                 "meetup" : new_question["meetup"]
+                }
+            ]
+            }
+        )
