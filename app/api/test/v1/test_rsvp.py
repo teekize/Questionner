eeb2e4d8a5252feacf271fa235e4054c2ecab5c8
Teeka Elvis <elvisteeka1@gmail.com>
2019-01-15 15:55:27 +0300

    [163218188] added the teardown

--- app/api/test/v1/test_rsvp.py
+++ app/api/test/v1/test_rsvp.py
@@ -10,8 +10,6 @@ class TestsForRsvp(unittest.TestCase):
         self.app.testing=True
         self.client=self.app.test_client()
         self.rsvp={
-            
-            "meetup": 1,
             "user" : 1,
             "response" : "yes"
         }
@@ -19,23 +17,25 @@ class TestsForRsvp(unittest.TestCase):
             
             "name":"teeka",
             "location":"nairobi", 
-            "images" : "pintrest.png",
+            "image" : "pintrest.png",
             "topic" : "what is Andela",
-            "tags" : "dev",
-            "happeningOn" : "2019-1-10"
+            "tag" : "dev",
+            "happeningOn" : "2019-01-10 10:30"
+            
         }
 
     def test_create_rsvp(self):
         '''tests the creation of an rsvp'''
-        response=self.client.post("/api/v1/meetups", data = json.dumps(self.meetup),
+        response = self.client.post("/api/v1/meetups", data = json.dumps(self.meetup),
                                                     content_type = "application/json")
         results = json.loads(response.data.decode())
         resluts_id = results["data"][0]["id"]
         self.assertEqual(response.status_code, 201)
 
-        response = self.client.post("/api/v1/meetups/{}/rsvp".format(resluts_id), data= json.dumps(self.rsvp), 
-                                                                                  content_type ="application/json")
-        self.assertEqual(response.status_code, 201)
+        response_from_posting_rsvp = self.client.post("/api/v1/meetups/{}/rsvp".format(resluts_id), 
+                                                                data = json.dumps(self.rsvp), 
+                                                            content_type ="application/json")
+        self.assertEqual(response_from_posting_rsvp.status_code, 201)
 
 
     def tearDown(self):
