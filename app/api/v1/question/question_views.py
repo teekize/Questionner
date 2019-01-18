3e0c0ad704f0e8f485f0b2fd9b4a91c68cdaf370
Teeka Elvis <elvisteeka1@gmail.com>
2019-01-18 10:14:59 +0300

    [#163317212] added the validation to this class

--- app/api/v1/question/question_views.py
+++ app/api/v1/question/question_views.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, jsonify, request
 from app.api.v1.question.question_model import QuestionModel
+from app.api.v1.validation import ValidationModule
 import datetime
 
 question_blueprint=Blueprint("question_blueprint", __name__, url_prefix= "/api/v1")
 question = QuestionModel()
+validator = ValidationModule()
 
 def Validator(details):
     for value in details:
@@ -34,9 +36,13 @@ def post_question():
 
     details = body, createdBy, title
 
-    if Validator(details) == False:
+    if validator.check_if_string(details) == False:
         return jsonify({"status": 400,
-                        "error" : "invalid data"
+                        "error" : "input details need to be in the form of strings"
+                        }), 400
+    if validator.check_if_data_not_in_(details) == False:
+        return jsonify({"status": 400,
+                        "error" : "input details cannot be empty"
                         }), 400
 
     results = question.save(createdBy, meetup,title, body)
